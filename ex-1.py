583065133f17ffe5e7fb7f88e4d7f9c811bd5050
Tommy Ogino <tommyogino275@gmail.com>
2025-12-30 14:36:10 -0300

    first commit

---
+++ ex-1.py
@@ -0,0 +1,9 @@
+apples = int(input("Digite a quantidade de macas vendidas: "))
+bananas = int(input("Digite a quantidade de bananas vendidas: "))
+
+if apples > bananas:
+    print("As macas tiveram mais vendas.")
+elif bananas > macas:
+    print("As bananas tiveram mais vendas.")
+else:
+    print("As vendas foram iguais.")
