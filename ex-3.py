92fe14c711660bb3cbe43b6b45c66c51609080ca
Tommy Ogino <tommyogino275@gmail.com>
2025-12-31 14:20:28 -0300

    ex-3.py

---
+++ ex-3.py
@@ -0,0 +1,6 @@
+temp = float(input("Digite a temperatura atual (em graus Celcius): "))
+
+if temp > 25:
+    print("Alerta! Temperatura acima do limite permitido!")
+else:
+    print("Temperatura dentro do limite permitido!")
