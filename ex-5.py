c3ac6e5d4626743207c047037c0e7cea173ef6fb
Tommy Ogino <tommyogino275@gmail.com>
2025-12-31 19:34:12 -0300

    ex-5.py

---
+++ ex-5.py
@@ -0,0 +1,6 @@
+gastos = float(input("Digite os gastos do mes: RS "))
+
+if gastos > 3000:
+	print("Atencao! Vc ultrapassou o limite de orcamento!")
+else:
+	print("Parabens! Vc esta dentro do limite do orcamento!")
